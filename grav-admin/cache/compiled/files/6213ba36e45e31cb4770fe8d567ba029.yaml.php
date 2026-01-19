f330b90441cd1d38687b376450e40256e028df26
guomangu <gu@DESKTOP-O5EP7DG.localdomain>
2026-01-19 02:00:36 +0100

    room access clear commit

---
+++ grav-admin/cache/compiled/files/6213ba36e45e31cb4770fe8d567ba029.yaml.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/home/gu/g2/grav-admin/user/accounts/guang.yaml',
+    'modified' => 1768777407,
+    'size' => 346,
+    'data' => [
+        'state' => 'enabled',
+        'email' => 'guang@gmail.com',
+        'fullname' => 'Guang',
+        'title' => 'Membre',
+        'access' => [
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$12$XCnwykGoMafw.53.nrmYYu2AMdvENH5bhqBRyz5mDWhqP7B.x3aAG',
+        'relations' => [
+            'spaces' => [
+                0 => 'rooom',
+                1 => 'roooom-boy',
+                2 => 'ytreza',
+                3 => 'dfghjk',
+                4 => 'sdfghj',
+                5 => 'hgfds',
+                6 => 'wlallaw',
+                7 => 'masuepdld',
+                8 => 'treza',
+                9 => 'azerty',
+                10 => 'eciec'
+            ]
+        ]
+    ]
+];
